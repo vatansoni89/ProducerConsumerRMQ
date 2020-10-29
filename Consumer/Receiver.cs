f4741a4814084339c096260fe6f5ebdc95c0ab5b
Vatan Soni <vatansoni89@gmail.com>
2020-10-29 19:18:50 +0530

    Basic producer consumer

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,36 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Channels;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    class Receiver
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Received Message {0}...",message);
+                };
+
+                channel.BasicConsume("BasicTest",true,consumer);
+                Console.WriteLine("Press [enter] to exit Consumer...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Producer/Sender.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http.Headers;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Producer
+{
+    public class Sender
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory(){HostName = "localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                string message = "Getting started RabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("","BasicTest",null,body);
+                Console.WriteLine("Sent message {0}...", message);
+            }
+
+            Console.WriteLine("Press [enter] to exit sender app");
+            Console.ReadLine();
+        }
+    }
+}
